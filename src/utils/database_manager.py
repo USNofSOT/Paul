4318593a65b9bca13de0a8a53a0f217d26548041
Terin <mlipnic@gmail.com>
2024-10-09 16:25:51 -0400

    Updated folder structure.

--- src/utils/database_manager.py
+++ src/utils/database_manager.py
@@ -1,14 +1,9 @@
 #imports
 
-import asyncio
-import datetime
-import discord
+
 import mariadb
 import os
 
-from datetime import datetime, timezone
-from discord import Intents, Client, Message
-from discord.ext.commands import Bot
 from dotenv import load_dotenv
 from typing import Final
 
