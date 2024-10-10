5ea64dd514637ea861ef5b463592adde260d1695
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-10 15:13:37 +0200

    feat: make new entry point for database connection engine.py

---
+++ src/database/engine.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import Engine, create_engine
+
+load_dotenv(override=True)
+log = logging.getLogger(__name__)
+
+db_config = {
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+}
+
+# Initialize the engine used to connect to the database
+# See: https://docs.sqlalchemy.org/en/20/core/engines.html
+engine_string = f"mysql+mariadbconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}"
+engine: Engine = create_engine(engine_string)
