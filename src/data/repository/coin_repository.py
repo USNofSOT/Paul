89ab6da824ff70e3c84a920abf6d3fdc30f047ca
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-12 23:32:10 +0200

    feat: turn coin repository in class managed as well

--- src/data/repository/coin_repository.py
+++ src/data/repository/coin_repository.py
@@ -9,29 +9,38 @@ from src.data.models import Coins
 log = logging.getLogger(__name__)
 Session = sessionmaker(bind=engine)
 
+class CoinRepository:
+    def __init__(self):
+        self.session = Session()
 
-def save_coin(target_id: int, coin_type: str, moderator_id: int, old_name: str, coin_time: datetime = datetime.now()) -> Coins:
-    """
-    Save a coin transaction to the database.
+    def get_session(self):
+        return self.session
 
-    Args:
-        target_id (int): The Discord ID of the user.
-        coin_type (str): The type of coin transaction.
-        moderator_id (int): The Discord ID of the moderator.
-        old_name (str): The old name of the user.
-        coin_time (datetime): The time of the transaction.
-    Returns:
-        Coins: The Coins object
-    """
-    session = Session()
-    try:
-        coin = Coins(target_id=target_id, coin_type=coin_type, moderator_id=moderator_id, old_name=old_name, coin_time=coin_time)
-        session.add(coin)
-        session.commit()
-        return coin
-    except Exception as e:
-        log.error(f"Error adding coin: {e}")
-        session.rollback()
-        raise e
-    finally:
-        session.close()
+    def close_session(self):
+        self.session.close()
+
+    def save_coin(self, target_id: int, coin_type: str, moderator_id: int, old_name: str, coin_time: datetime = datetime.now()) -> Coins:
+        """
+        Save a coin transaction to the database.
+
+        Args:
+            target_id (int): The Discord ID of the user.
+            coin_type (str): The type of coin transaction.
+            moderator_id (int): The Discord ID of the moderator.
+            old_name (str): The old name of the user.
+            coin_time (datetime): The time of the transaction.
+        Returns:
+            Coins: The Coins object
+        """
+
+        try:
+            coin = Coins(target_id=target_id, coin_type=coin_type, moderator_id=moderator_id, old_name=old_name, coin_time=coin_time)
+            self.session.add(coin)
+            self.session.commit()
+            return coin
+        except Exception as e:
+            log.error(f"Error adding coin: {e}")
+            self.session.rollback()
+            raise e
+        finally:
+            self.session.close()
