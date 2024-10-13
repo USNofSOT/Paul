3e0568dfaaf88406915758e451f5a17ed966cddd
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-13 17:22:56 +0200

    feat: properly close repositories when done on_load_voyage.py

--- src/cogs/on_load_voyage.py
+++ src/cogs/on_load_voyage.py
@@ -22,18 +22,26 @@ class On_Load_Voyages(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        voyage_repository = VoyageRepository()
+        hosted_repository = HostedRepository()
+        sailor_repository = SailorRepository()
+
         try:
             log.info("Processing existing voyage logs.")
 
+
             channel = self.bot.get_channel(VOYAGE_LOGS)
             async for message in channel.history(limit=50, oldest_first=False):  # Fetch the last 50
-                await Process_Voyage_Log.process_voyage_log(message, VoyageRepository(), HostedRepository(), SailorRepository())
+                await Process_Voyage_Log.process_voyage_log(message, voyage_repository, hosted_repository, sailor_repository)
                 await asyncio.sleep(0.5)  # Introduce a 1second delay to prevent blocking
                 #print(f"Processed log: {message.id}.")  #enable this line if you need to view the processing as it happens.
         except Exception as e:
             log.error(f"Error processing existing voyage logs: {e}")
         finally:
             log.info("Finished processing existing voyage logs.")
+            voyage_repository.close_session()
+            hosted_repository.close_session()
+            sailor_repository.close_session()
 
 
 async def setup(bot: commands.Bot):
