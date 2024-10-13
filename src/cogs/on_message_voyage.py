5ea7dfc8ad63d6ec7701071af7abb0dc07f788d9
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-13 17:23:37 +0200

    feat: do the same for onmessage

--- src/cogs/on_message_voyage.py
+++ src/cogs/on_message_voyage.py
@@ -6,6 +6,10 @@ from discord import app_commands
 from config import VOYAGE_LOGS
 from utils.process_voyage_log import Process_Voyage_Log
 
+from src.data.repository.hosted_repository import HostedRepository
+from src.data.repository.sailor_repository import SailorRepository
+from src.data.repository.voyage_repository import VoyageRepository
+
 # from utils.database_manager import DatabaseManager   # Imports Database Manager from Utilies if needed uncomment it!
 
 log = getLogger(__name__)
@@ -18,10 +22,21 @@ class On_Message_Process_voyage(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        voyage_repository = VoyageRepository()
+        hosted_repository = HostedRepository()
+        sailor_repository = SailorRepository()
         # Voyage Channel Work
         if message.channel.id == int(VOYAGE_LOGS):
-            await Process_Voyage_Log.process_voyage_log(message)
-            log.info(f"[{message.id}] Voyage log processed.")
+            try:
+                await Process_Voyage_Log.process_voyage_log(message, voyage_repository, hosted_repository,
+                                                            sailor_repository)
+                log.info(f"[{message.id}] Voyage log processed.")
+            except Exception as e:
+                log.error(f"Error processing voyage log: {e}")
+            finally:
+                voyage_repository.close_session()
+                hosted_repository.close_session()
+                sailor_repository.close_session()
 
 
         
