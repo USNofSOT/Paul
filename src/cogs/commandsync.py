8c7e943ce2315359722efd24352d6e68bfdcd99d
Terin <mlipnic@gmail.com>
2024-10-15 13:00:52 -0400

    Updated command sync to have security for NSC department.

--- src/cogs/commandsync.py
+++ src/cogs/commandsync.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
+from config import NSC_ROLE
 
 class CommandSync(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="commandsync")
+    @commands.has_any_role(NSC_ROLE)
     async def commandsync(self, ctx):
         """Syncs the application commands."""
         await self.bot.tree.sync()
