5fa99ed8ca16e3a2bf59af7fa592553ac05ddeca
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-15 18:26:32 +0200

    feat: create !logs command

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import os
 from datetime import datetime
@@ -14,18 +15,20 @@ def initialise_logger():
         log.info(f'Logs directory not found, creating directory: {LOGS_DIR}')
         os.makedirs(LOGS_DIR)
 
+    today = datetime.now().strftime('%Y-%m-%d')
     # General log handler
     general_handler = RotatingFileHandler(
-        filename=f'{LOGS_DIR}/BOT-{int(datetime.now().timestamp())}.log',
+        filename=f'{LOGS_DIR}/BOT-{today}.log',
         maxBytes=5 * 1024 * 1024,  # 5MB
-        backupCount=10,  # Keep 10 backup files
+        backupCount=5,  # Keep 10 backup files
+        encoding='utf-8',
     )
 
     # Error log handler
     error_handler = RotatingFileHandler(
         filename=f'{LOGS_DIR}/BOT-ERROR-{int(datetime.now().timestamp())}.log',
         maxBytes=5 * 1024 * 1024,  # 5MB
-        backupCount=25,  # Keep 25 backup files
+        backupCount=5,  # Keep 25 backup files
     )
     error_handler.setLevel(logging.ERROR)
 
@@ -35,3 +38,17 @@ def initialise_logger():
         format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
     )
     log.info('Logger initialised')
+
+def get_most_recent_log_file():
+    log_files = glob.glob(os.path.join(LOGS_DIR, 'BOT-[!ERROR]*.log'))
+    if not log_files:
+        return None
+    most_recent_log = max(log_files, key=os.path.getctime)
+    return most_recent_log
+
+def get_most_recent_error_log_file():
+    error_log_files = glob.glob(os.path.join(LOGS_DIR, 'BOT-ERROR-*.log'))
+    if not error_log_files:
+        return None
+    most_recent_error_log = max(error_log_files, key=os.path.getctime)
+    return most_recent_error_log
