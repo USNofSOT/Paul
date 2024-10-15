e43293627b3b667e0991bc79b805c4645698263e
Terin <mlipnic@gmail.com>
2024-10-15 09:13:36 -0400

    Reworked on edit and on delete, tested

--- src/cogs/on_delete_voyage.py
+++ src/cogs/on_delete_voyage.py
@@ -1,36 +1,37 @@
 from logging import getLogger
 
 from discord.ext import commands
+from discord import RawMessageDeleteEvent
 
-from src.config import VOYAGE_LOGS
-from src.data.repository.hosted_repository import remove_hosted_entry_by_log_id
+from src.config import VOYAGE_LOGS, GUILD_ID
+from src.data.repository.hosted_repository import remove_hosted_entry_by_log_id, HostedRepository
 from src.data.repository.sailor_repository import decrement_voyage_count_by_discord_id, \
     decrement_hosted_count_by_discord_id
 from src.data.repository.subclass_repository import SubclassRepository
-from src.data.repository.voyage_repository import remove_voyage_log_entries
+from src.data.repository.voyage_repository import remove_voyage_log_entries, VoyageRepository
 
 log = getLogger(__name__)
 
 class On_Delete_Voyages(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload):
+        self.subclass_repository = SubclassRepository()
+        self.hosted_repository = HostedRepository()
+        self.voyage_repository = VoyageRepository()
         if payload.channel_id == VOYAGE_LOGS:
-            log_id = payload.message_id
-
-            # If the message is cached, you can access it
-            message = self.bot.get_message(log_id) 
-            if message:
-                host_id = message.author.id
-                participant_ids = [user.id for user in message.mentions]
+            log_id = payload.message_id                 
+            host = self.hosted_repository.get_host_by_log_id(log_id)
+            if host:
+                host_id = host.discord_id
             else:
-                # If not cached, you'll need to fetch details from the database
-                # ... (Logic to retrieve host_id and participant_ids based on log_id)
-
-            subclass_repository = SubclassRepository()
-
+                log.info(f"Voyage:{log_id} was not logged before.")
+                return
+            participant_ids = [user.discord_id for  user in self.voyage_repository.get_sailors_by_log_id(log_id)]
+            
             log.info(f"[{log_id}] Message deleted in voyage log channel.")
 
             # Decrement hosted count
@@ -39,7 +40,7 @@ class On_Delete_Voyages(commands.Cog):
             log.info(f"[{log_id}] Removed hosted entry for: {host_id}")
 
             # Remove subclass entries
-            subclass_entries = subclass_repository.delete_all_subclass_entries_for_log_id(log_id)
+            subclass_entries = self.subclass_repository.delete_all_subclass_entries_for_log_id(log_id)
 
             # Decrement voyage counts for participants
             for participant_id in participant_ids:
@@ -49,6 +50,10 @@ class On_Delete_Voyages(commands.Cog):
             # Remove entries from VoyageLog table (if necessary)
             remove_voyage_log_entries(log_id)
             log.info(f"[{log_id}] Voyage log entries removed.")
+
+        self.subclass_repository.close_session()
+        self.hosted_repository.close_session()
+        self.voyage_repository.close_session()
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(On_Delete_Voyages(bot))  # Classname(bot)
