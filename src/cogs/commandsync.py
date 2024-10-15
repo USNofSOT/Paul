d8793a14849cae807666a8b8aa3570bef98aaf85
Terin <mlipnic@gmail.com>
2024-10-15 12:51:03 -0400

    HOTFIX: Adding command !commandsync

---
+++ src/cogs/commandsync.py
@@ -0,0 +1,14 @@
+from discord.ext import commands
+
+class CommandSync(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="commandsync")
+    async def commandsync(self, ctx):
+        """Syncs the application commands."""
+        await self.bot.tree.sync()
+        await ctx.send("Application commands synced!")
+
+async def setup(bot):
+    await bot.add_cog(CommandSync(bot))
