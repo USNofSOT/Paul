6a8cb582cb94339e1c16edb245d17a37e01d6cd1
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-10-17 11:29:50 +0200

    fix: have an interaction log

--- src/core/bot.py
+++ src/core/bot.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
 
-from typing import Optional
-from discord.ext import commands
 from logging import getLogger
+from typing import Optional
 
-from discord.ext.commands import ExtensionNotFound, NoEntryPointError, ExtensionFailed
+from discord.ext import commands
 
 from src.cogs import EXTENTIONS
-from src.config import ENGINE_ROOM, SPD_ID
 
 log= getLogger(__name__)
-import discord, os
-
+import discord
 
 __all__ = (
     "Bot",
@@ -46,6 +43,10 @@ class Bot(discord.ext.commands.Bot):
     async def   error(self, content: str, interaction: discord.Interaction, ephemeral: Optional[bool]):
         """Sending error Message"""
         pass
+
+    async def on_interaction(self, interaction: discord.Interaction):
+        if interaction.type == discord.InteractionType.application_command:
+            log.info(f" > [INTERACTION] by {interaction.user} in {interaction.channel} : {interaction.data}")
 
     async def setup_hook(self):
         log.info(f"Loading {len(EXTENTIONS)} extensions")
