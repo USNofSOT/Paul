0f7bc171dd605694f4e1bcacf8c17518acf6a2a7
Kip Hart <kiphart91@gmail.com>
2024-10-18 16:27:30 -0700

    moved config to folder

--- src/old_config.py
+++ src/old_config.py
@@ -3,6 +3,8 @@ from typing import Final
 from dotenv import load_dotenv
 import os
 
+from data.structs import Award
+
 load_dotenv()
 
 # TOKEN
@@ -61,6 +63,40 @@ JO_ROLE = 933916865976213514 # Junior Officer
 SO_ROLE = 933916762850877491 # Senior Officer
 BOA_ROLE = 933914385099923497 # Board of Admirality
 
+E1_ROLES = [933913081099214848] # Recruit
+E2_ROLES = [933913010806857801, 1201933589357547540] # Seaman, Seaman Apprentice
+E3_ROLES = [933912647999565864] # Able Seaman
+E4_ROLES = [933912557008343120] # Junior Petty Officer
+E5_ROLES = [] # Unused
+E6_ROLES = [933911949585035275] # Petty Officer
+E7_ROLES = [933911464660570132] # Chief Petty Officer
+E8_ROLES = [933911373669335092] # Senior Chief Petty Officer
+O1_ROLES = [933910558695129118] # Midshipman
+O2_ROLES = [] # Unused
+O3_ROLES = [933910174555598879] # Lieutenant
+O4_ROLES = [933909957437423677] # Lieutenant Commander
+O5_ROLES = [933909780639150101] # Commander
+O6_ROLES = [933909668550553630] # Captain
+O7_ROLES = [933909182711746570] # Commodore
+O8_ROLES = [1157429131416449134] # Rear Admiral
+
+O8_AND_UP = O8_ROLES + [BOA_ROLE] # some overlap, but includes BOA members without ranks
+O7_AND_UP = O7_ROLES + O8_AND_UP
+O6_AND_UP = O6_ROLES + O7_AND_UP
+O5_AND_UP = O5_ROLES + O6_AND_UP
+O4_AND_UP = O4_ROLES + O5_AND_UP
+O3_AND_UP = O3_ROLES + O4_AND_UP
+O2_AND_UP = O2_ROLES + O3_AND_UP
+O1_AND_UP = O1_ROLES + O2_AND_UP
+E8_AND_UP = E8_ROLES + O1_AND_UP
+E7_AND_UP = E7_ROLES + E8_AND_UP
+E6_AND_UP = E6_ROLES + E7_AND_UP
+E5_AND_UP = E5_ROLES + E6_AND_UP
+E4_AND_UP = E4_ROLES + E5_AND_UP
+E3_AND_UP = E3_ROLES + E4_AND_UP
+E2_AND_UP = E2_ROLES + E3_AND_UP
+E1_AND_UP = E1_ROLES + E2_AND_UP
+
 # Departments
 NRC_ROLE = 944631719456284735 # NRC Department
 NSC_ROLE = 1293725126562680985 # NSC Department
@@ -84,3 +120,58 @@ SNCO_AND_UP = SNCO_ROLE, JO_ROLE, SO_ROLE, BOA_ROLE, NSC_ROLE
 JO_AND_UP = JO_ROLE, SO_ROLE, BOA_ROLE, NSC_ROLE
 SO_AND_UP = SO_ROLE, BOA_ROLE, NSC_ROLE
 BOA_NSC = BOA_ROLE, NSC_ROLE
+
+# AWARDS INFO
+## Medals And Ribbons - Voyage Medals
+CITATION_OF_VOAYGES = Award(
+    role_id=934090102735536188,
+    embed_id=1292514303936561183,
+    ranks_responsible="E-6+",
+    threshold=5
+)
+LEGION_OF_VOYAGES = Award(
+    role_id=983412707912998942,
+    embed_id=1292514303936561183,
+    ranks_responsible="E-7+",
+    threshold=25
+)
+VOYAGE_MEDALS = (CITATION_OF_VOAYGES, LEGION_OF_VOYAGES)
+
+MEDALS_AND_RIBBONS = {
+    'voyages': VOYAGE_MEDALS,
+}
+'''
+CITATION_OF_VOAYGES = {
+    'minimum': 5,
+    'ranks responsible': E6_AND_UP,
+    'role': 934090102735536188,
+}
+LEGION_OF_VOYAGES = {
+    'minimum': 25,
+    'ranks responsible': E7_AND_UP,
+    'role': 983412707912998942,
+}
+HONORABLE_VOYAGER_MEDAL = {
+    'minimum': 50,
+    'ranks responsible': O1_AND_UP,
+    'role': 1059271717836566588,
+}
+MERITORIOUS_VOYAGER_MEDAL = {
+    'minimum': 100,
+    'ranks responsible': O4_AND_UP,
+    'role': 1059240151852797993,
+}
+ADMIRABLE_VOYAGER_MEDAL = {
+    'minimum': 200,
+    'ranks responsible': O7_AND_UP,
+    'role': 1140637598457544857,
+}
+# Medals must be ordered lowest to highest voyage count
+VOYAGE_MEDALS = (CITATION_OF_VOAYGES, LEGION_OF_VOYAGES, HONORABLE_VOYAGER_MEDAL, 
+                 MERITORIOUS_VOYAGER_MEDAL, ADMIRABLE_VOYAGER_MEDAL)
+
+
+MEDALS_AND_RIBBONS = {
+    'voyages': VOYAGE_MEDALS,
+}
+'''
