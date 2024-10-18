e86729fd1799a0b06f3474eae3b346c624b41853
Kip Hart <kiphart91@gmail.com>
2024-10-18 16:27:30 -0700

    moved config to folder

---
+++ src/config/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from dotenv import load_dotenv
+
+from config.awards import *
+from config.main_server import *
+from config.ranks_roles import *
+from config.spd_servers import *
+from config.synonyms import *
+
+load_dotenv()
+
+NSC_ROLES = NSC_ROLE, SPD_NSC_ROLE
+
+
+
---
+++ src/config/synonyms.py
@@ -0,0 +1,7 @@
+# Subclass Synonyms
+CARPENTER_SYNONYMS = ['carpenter', 'carp', 'bilge']
+FLEX_SYNONYMS = ['flex', 'flexer', 'boarder']
+CANNONEER_SYNONYMS = ['cannoneer', 'cannon', 'gunner', 'canonneer', 'cannons', 'mc']
+HELM_SYNONYMS = ['helm', 'helmsman', 'navigator']
+SURGEON_SYNONYMS = ['surgeon', 'doc', 'medic', 'field surgeon']
+GRENADIER_SYNONYMS = ['grenadier', 'kegger', 'bomber']
