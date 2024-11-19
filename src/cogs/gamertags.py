b8fbc6db9ade15797e175c6911c49f9ccefcf2de
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-11-19 18:15:39 +0100

    feat: add ship gamertags.py stuff

--- src/cogs/gamertags.py
+++ src/cogs/gamertags.py
@@ -16,11 +16,34 @@ class Gamertags(commands.Cog):
         self.bot = bot
 
     @app_commands.command(name="gamertags", description="Get the gamertags of all users in the voice channel of the executing user")
+    @app_commands.describe(target="Select a role to get the gamertags of all users in the voice channel of the executing user")
     @app_commands.checks.has_any_role(*NCO_AND_UP)
-    async def gamertags(self, interaction: discord.Interaction):
+    async def gamertags(self, interaction: discord.Interaction, target: discord.Role = None):
         """
         Get the gamertags of all users in the voice channel of the executing user.
         """
+        if target:
+            if len(target.members) > 30:
+                await interaction.response.send_message(embed=error_embed(title="Error", description="Too many members with the selected role."), ephemeral=True)
+                return
+
+            embed = default_embed(title="Gamertags overview",
+                                  description=f"The gamertags of all users with the role {target.mention}")
+            user_strings = ""
+            sailor_repository = SailorRepository()
+            for member in target.members:
+                sailor = sailor_repository.get_sailor(member.id)
+                if sailor is not None and sailor.gamertag is not None:
+                    user_strings += f"- <@{member.id}>: {sailor.gamertag}\n"
+                else:
+                    user_strings += f"- <@{member.id}>: No gamertag set\n"
+
+            embed.add_field(name="Gamertags", value=user_strings)
+            await interaction.response.send_message(embed=embed)
+            sailor_repository.close_session()
+            return
+
+
         member = interaction.user
 
         if member.voice is None:
