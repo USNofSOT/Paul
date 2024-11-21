d46d076264a6675a577d05518180871e5b8c7491
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-11-21 12:20:05 +0100

    feat: possibly log bans and removes

---
+++ src/cogs/on_member_remove.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+
+from src.config.ranks_roles import E2_AND_UP
+from src.data.repository.auditlog_repository import AuditLogRepository
+
+
+class OnMemberRemove(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        audit_log_repository = AuditLogRepository()
+
+        member_roles = [role.id for role in member.roles]
+        e2_or_above = any(role in member_roles for role in E2_AND_UP)
+
+        audit_log_repository.log_member_removed(member.id, member.guild.id, e2_or_above)
+
+        audit_log_repository.close_session()
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(OnMemberRemove(bot))
