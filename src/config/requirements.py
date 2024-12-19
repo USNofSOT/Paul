ff83faf89fc9cc285ba494571487fb4af971ecf2
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2024-12-19 10:57:03 +0100

    fix: hosting/voyaging requirements.py into a configuration file

---
+++ src/config/requirements.py
@@ -0,0 +1,3 @@
+# Within how many days should one participate in a voyage or host a voyage?
+HOSTING_REQUIREMENT_IN_DAYS = 21 # 21 days = 3 weeks
+VOYAGING_REQUIREMENT_IN_DAYS = 28 # 28 days = 4 weeks
