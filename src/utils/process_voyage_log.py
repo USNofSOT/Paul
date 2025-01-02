c9d34b492fa1e78ce4b0e977b48eb07d37b06ece
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-01-02 16:53:47 +0100

    fix: doubloon counter

--- src/utils/process_voyage_log.py
+++ src/utils/process_voyage_log.py
@@ -31,7 +31,7 @@ def get_gold_count_from_content(content: str) -> int:
 
 def get_doubloon_count_from_content(content: str) -> int:
     pattern = r"(?i)(?:doubloons[:> a-z]*\s*(\d+)|(\d+)\s*[:< a-z]doubloons)"
-    text_without_ids = re.sub(r':\d+>', '>', content)
+    text_without_ids = re.sub(r':\d+>', '>', content.replace(",", "").replace(".", "").replace("<", ""))
     match = re.search(pattern, text_without_ids)
     return min(int(match.group(1) or match.group(2)), 20000000) if match else 0
 
