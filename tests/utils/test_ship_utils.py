ccbe484b3e518765c0acc13b5d52d1bff306b9ff
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-01-13 13:28:20 +0100

    feat: add USS-SHIP_NAME test case

--- tests/utils/test_ship_utils.py
+++ tests/utils/test_ship_utils.py
@@ -90,6 +90,12 @@ class TestGetMainShipName(TestCase):
         content = "We started our adventure at Plunder on the we dit a lot of yapping at least a good 25 words, so that is like more than 25 words. USS Phoenix"
         self.assertEqual(get_main_ship_from_content(content), None)
 
+    def test_main_ship_with_dash_between_words(self):
+        content = "We started our adventure at Plunder on the USS Phoenix, Auxiliary to the USS Platypus"
+        self.assertEqual(get_main_ship_from_content(content), "USS Platypus")
+        content = "We started our adventure at Plunder on the USS Phoenix, Auxiliary to the USS-Platypus"
+        self.assertEqual(get_main_ship_from_content(content), None)
+
 class TestGetAuxiliaryShipName(TestCase):
 
     def test_auxiliary_ship_included(self):
@@ -112,4 +118,10 @@ class TestGetAuxiliaryShipName(TestCase):
         content = "We started our adventure at Plunder on the USS Phoenix, Auxiliary to the USS Platypus"
         self.assertEqual(get_auxiliary_ship_from_content(content), "USS Phoenix")
         content = "We started our adventure at Plunder on the USS Platypus we dit a lot of yapping at least a good 25 words, so that is like more than 25 words. USS Phoenix"
+        self.assertEqual(get_auxiliary_ship_from_content(content), None)
+
+    def test_auxiliary_ship_with_dash_between_words(self):
+        content = "We started our adventure at Plunder on the USS Phoenix, Auxiliary to the USS Platypus"
+        self.assertEqual(get_auxiliary_ship_from_content(content), "USS Phoenix")
+        content = "We started our adventure at Plunder on the USS Phoenix, Auxiliary to the USS-Platypus"
         self.assertEqual(get_auxiliary_ship_from_content(content), None)
