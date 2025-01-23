bab643f6d7efacdbc39ff172e3485f6dcf67dc70
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-01-23 17:16:13 +0100

    fix: add subclass warnings

--- src/config/requirements.py
+++ src/config/requirements.py
@@ -1,3 +1,6 @@
 # Within how many days should one participate in a voyage or host a voyage?
 HOSTING_REQUIREMENT_IN_DAYS = 14 # 14 days = 2 weeks
 VOYAGING_REQUIREMENT_IN_DAYS = 28 # 28 days = 4 weeks
+
+# Gold required to secure a patrol
+MINIMUM_GOLD_REQUIREMENT_FOR_PATROL = 75000
