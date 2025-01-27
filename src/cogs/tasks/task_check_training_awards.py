b9a45497c21295fece0dc7b0caac44af8ebcb4aa
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-01-27 14:59:08 +0100

    feat: add test for NETC awards notifications

--- src/cogs/tasks/task_check_training_awards.py
+++ src/cogs/tasks/task_check_training_awards.py
@@ -1,12 +1,21 @@
+import datetime
 from logging import getLogger
 
 from cogs.tasks.task_check_awards import fake_context
-from config import GUILD_ID, NETC_ROLE, NRC_ROLE
+from config import (
+    CO_OF_NETC_ROLE,
+    GUILD_ID,
+    NETC_BOT_CHANNEL,
+    NETC_GUILD_ID,
+    NETC_ROLE,
+    NRC_ROLE,
+)
 from config.ranks import DECKHAND, RETIRED, VETERAN
 from data import TrainingRecord
 from data.repository.training_records_repository import TrainingRecordsRepository
 from discord.ext import commands, tasks
 from utils.check_awards import check_training
+from utils.discord_utils import alert_engineers
 
 log = getLogger(__name__)
 
@@ -22,64 +31,76 @@ class AutoCheckAwardsTraining(commands.Cog):
         self.my_task.cancel()
 
     # TODO: Move this into configuration as well
-    @tasks.loop(minutes=1)
+    @tasks.loop(time=datetime.time(hour=15, minute=00, tzinfo=datetime.timezone.utc))
     async def my_task(self):
-        print("Checking awards")
+        log.info("Checking training awards")
         training_repository = TrainingRecordsRepository()
-        channel = self.bot.get_guild(GUILD_ID).get_channel(1291589569602650154)
 
-        # Get all training records
-        training_records = training_repository.get_session().query(TrainingRecord).all()
+        try:
+            nrc_channel = self.bot.get_guild(GUILD_ID).get_channel(1291589569602650154)
+            netc_channel = self.bot.get_guild(NETC_GUILD_ID).get_channel(NETC_BOT_CHANNEL)
 
-        # Filter out records based on conditions
-        guild = self.bot.get_guild(GUILD_ID)
-        training_records = [
-            record for record in training_records
-            if guild.get_member(record.target_id) is not None and
-               DECKHAND.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles] and
-               VETERAN.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles] and
-               RETIRED.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles]
-        ]
+            # Get all training records
+            training_records = training_repository.get_session().query(TrainingRecord).all()
 
-        nrc_str = ""
-        netc_str = ""
+            # Filter out records based on conditions
+            guild = self.bot.get_guild(GUILD_ID)
+            training_records = [
+                record for record in training_records
+                if guild.get_member(record.target_id) is not None and
+                   DECKHAND.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles] and
+                   VETERAN.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles] and
+                   RETIRED.role_ids[0] not in [role.id for role in guild.get_member(record.target_id).roles]
+            ]
 
-        for training_record in training_records:
-            member = self.bot.get_guild(GUILD_ID).get_member(training_record.target_id)
+            nrc_str = ""
+            netc_str = ""
 
-            in_nrc = member is not None and NRC_ROLE in [role.id for role in member.roles]
-            in_netc = member is not None and NETC_ROLE in [role.id for role in member.roles]
+            for training_record in training_records:
+                member = self.bot.get_guild(GUILD_ID).get_member(training_record.target_id)
 
-            sailor_strs = check_training(self.bot, fake_context(self.bot, ""), training_record, member)
-            if sailor_strs:
-                # If person is part of NRC Department and not NETC Department dedicates to NRC
-                if in_nrc and not in_netc:
-                    nrc_str += f"{''.join(sailor_strs)}"
-                # If person is part of NETC Department and not NRC Department dedicates to NETC
-                elif in_netc and not in_nrc:
-                    netc_str += f"{''.join(sailor_strs)}"
-                # If the person is part of both departments or neither, go by majority of points
-                else:
-                    nrc_points = training_record.nrc_training_points + training_record.st_training_points
-                    netc_points = training_record.netc_training_points
-                    if nrc_points > netc_points:
+                in_nrc = member is not None and NRC_ROLE in [role.id for role in member.roles]
+                in_netc = member is not None and NETC_ROLE in [role.id for role in member.roles]
+
+                sailor_strs = check_training(self.bot, fake_context(self.bot, ""), training_record, member)
+                if sailor_strs:
+                    # If person is part of NRC Department and not NETC Department dedicates to NRC
+                    if in_nrc and not in_netc:
                         nrc_str += f"{''.join(sailor_strs)}"
-                    elif netc_points > nrc_points:
+                    # If person is part of NETC Department and not NRC Department dedicates to NETC
+                    elif in_netc and not in_nrc:
                         netc_str += f"{''.join(sailor_strs)}"
-                    # If points are equal, pick randomly
+                    # If the person is part of both departments or neither, go by majority of points
                     else:
-                        if member.id % 2 == 0:
+                        nrc_points = training_record.nrc_training_points + training_record.st_training_points
+                        netc_points = training_record.netc_training_points
+                        if nrc_points > netc_points:
                             nrc_str += f"{''.join(sailor_strs)}"
-                        else:
+                        elif netc_points > nrc_points:
                             netc_str += f"{''.join(sailor_strs)}"
+                        # If points are equal, pick randomly
+                        else:
+                            if member.id % 2 == 0:
+                                nrc_str += f"{''.join(sailor_strs)}"
+                            else:
+                                netc_str += f"{''.join(sailor_strs)}"
 
-        if nrc_str:
-            await channel.send(f"**Pending Training Awards for NRC Department**\n{nrc_str}")
-        if netc_str:
-            await channel.send(f"**Pending Training Awards for NETC Department**\n{netc_str}")
+            if nrc_str:
+                log.info(f"Send NRC Training Awards to channel #{nrc_channel.name} in {nrc_channel.guild.name}")
+                await nrc_channel.send(f"**Pending Training Awards for NRC Department**\n{nrc_str}")
+            if netc_str:
+                log.info(f"Send NETC Training Awards to channel #{netc_channel.name} in {netc_channel.guild.name}")
+                await netc_channel.send(f"**Pending Training Awards for NETC Department (<@&{CO_OF_NETC_ROLE}>)**\n{netc_str}")
 
-
-        training_repository.close_session()
+        except Exception as e:
+            log.error(f"Error in AutoCheckAwards: {e}", exc_info=True)
+            await alert_engineers(
+                self.bot,
+                f"Error in AutoCheckAwards: {e}",
+                exception=e
+            )
+        finally:
+            training_repository.close_session()
 
 
 
