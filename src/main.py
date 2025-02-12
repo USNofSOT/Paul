3b2083f0c11fb4b15f6730aab2ed744140b674d5
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-02-12 18:34:49 +0100

    hotfix: alembic config

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from logging import getLogger
 
 import config
 import discord
+from core import Bot
 from data.engine import engine_string
 from data.migrations.migrate import run_migrations
 
 from src.data import create_tables
 from src.utils.logger import initialise_logger
 
-log= getLogger(__name__)
-from core import Bot
+log = getLogger(__name__)
 
 
 async def main():
@@ -26,11 +26,8 @@ async def main():
             await bot.process_commands(message)  # Process commands here
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_tables()
     initialise_logger()
-    run_migrations(
-        "../src/data/migrations",
-        engine_string
-    )
+    run_migrations(engine_string)
     asyncio.run(main())
