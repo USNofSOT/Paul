dabf6651e69e94510e8dc958d4948c68953085f6
Kip Hart <kiphart91@gmail.com>
2025-02-13 06:30:49 -0800

    added E-2 and 3 to the CoC config

--- src/data/structs.py
+++ src/data/structs.py
@@ -110,9 +110,9 @@ class SailorCO:
         co_set = False
         CoC = config.ranks_roles.CHAIN_OF_COMMAND
         CoC_keys = list(CoC.keys())
-        for idx, role_id in enumerate(CoC_keys):
+        for role_id in CoC_keys:
             if role_id in role_ids:
-                co_member = _get_co_from_link(idx, sailor, CoC, CoC_keys, guild)
+                co_member = _get_co_from_link(role_id, sailor, CoC, CoC_keys, guild)
                 self.immediate = co_member
                 co_set = True
                 break
@@ -151,8 +151,7 @@ class SailorCO:
             return f"<@{self.acting.id}>"
         return f"Current: <@{self.acting.id}>\nImmediate: <@{self.immediate.id}>"
 
-def _get_co_from_link(idx: int, sailor: Member, CoC: OrderedDict, CoC_keys: list[int], guild: Guild) -> Member | None:
-    role_id = CoC_keys[idx]
+def _get_co_from_link(role_id: int, sailor: Member, CoC: OrderedDict, CoC_keys: list[int], guild: Guild) -> Member | None:
     co_role_id, common_group = CoC[role_id]
     if co_role_id is None:
         return None
@@ -171,7 +170,7 @@ def _get_co_from_link(idx: int, sailor: Member, CoC: OrderedDict, CoC_keys: list
         return co_role_members[0]
     if len(co_role_members) == 0:
         # go up one in the CoC
-        return _get_co_from_link(idx-1, sailor, CoC, CoC_keys, guild)
+        return _get_co_from_link(co_role_id, sailor, CoC, CoC_keys, guild)
 
     warn("Found more than one member with role. Using first in list")
     return co_role_members[0]
