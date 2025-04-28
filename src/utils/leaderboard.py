eb7f072016b043271eb87d90feb809db2af95f7d
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2025-04-28 20:45:41 +0200

    chore: refactor leaderboard embed creation to use three columns for member display to prevent overflow or fields

--- src/utils/leaderboard.py
+++ src/utils/leaderboard.py
@@ -1,5 +1,6 @@
 import discord
 
+
 def create_leaderboard_embed(bot, guild_id, data, title):
     """
     Creates a leaderboard embed.
@@ -15,7 +16,7 @@ def create_leaderboard_embed(bot, guild_id, data, title):
     """
     embed = discord.Embed(title=f"__{title}__")
     guild = bot.get_guild(guild_id)
-    
+
     for rank, (member_id, value) in enumerate(data, 1):
         member = guild.get_member(member_id)  # Fetch the member object
         if member:
@@ -24,7 +25,6 @@ def create_leaderboard_embed(bot, guild_id, data, title):
         else:
             embed.add_field(name=f"{rank}. Unknown User", value=value, inline=False)
     return embed
-
 
 
 def create_master_embed(bot, guild_id, data, title):
@@ -43,38 +43,29 @@ def create_master_embed(bot, guild_id, data, title):
     embed = discord.Embed(title=f"__{title}__")
     guild = bot.get_guild(guild_id)
 
-    # Split the data into two columns
-    half_length = len(data) // 2
-    first_column = data[:half_length]
-    second_column = data[half_length:]
+    columns = [""] * 3
 
-    # Add members to the embed in two columns
-    for member_id_1, member_id_2 in zip(first_column, second_column):
-        member_1 = guild.get_member(member_id_1)
-        member_2 = guild.get_member(member_id_2)
-        if member_1 and member_2:
-            member_name_1 = member_1.display_name or member_1.name
-            member_name_2 = member_2.display_name or member_2.name
-            embed.add_field(name=f"{member_name_1}\u200b", value="\u200b", inline=True)
-            embed.add_field(name=f"{member_name_2}\u200b", value="\u200b", inline=True)
-        elif member_1:
-            member_name_1 = member_1.display_name or member_1.name
-            embed.add_field(name=member_name_1, value="\u200b", inline=True)
-        elif member_2:
-            member_name_2 = member_2.display_name or member_2.name
-            embed.add_field(name=member_name_2, value="\u200b", inline=True)
-
-    # If there's an odd number of members, add the last one
-    if len(data) % 2 != 0:
-        member_id = data[-1]
+    for index, member_id in enumerate(data):
         member = guild.get_member(member_id)
-        if member:
-            member_name = member.display_name or member.name
-            embed.add_field(name=member_name, value="\u200b", inline=True)
+        member_name = member.display_name if member else "Unknown User"
+        columns[index % 3] += f"{member_name}\n"
+
+    for column in columns:
+        embed.add_field(name="\u200b", value=column, inline=True)
 
     return embed
 
-def create_subclass_leaderboard_embed(bot, guild_id, top_helm, top_flex, top_cannoneer, top_carpenter, top_surgeon, top_grenadier):
+
+def create_subclass_leaderboard_embed(
+    bot,
+    guild_id,
+    top_helm,
+    top_flex,
+    top_cannoneer,
+    top_carpenter,
+    top_surgeon,
+    top_grenadier,
+):
     """
     Creates a leaderboard embed for subclass points.
 
@@ -105,7 +96,9 @@ def create_subclass_leaderboard_embed(bot, guild_id, top_helm, top_flex, top_can
                     member_name = member.display_name or member.name
                     # Add zero-width spaces to prevent wrapping
                     member_data.append(f"{member_name}\u200b:\u200b {value}")
-            embed.add_field(name=f"Top {subclass_name}", value="\n".join(member_data), inline=True)
+            embed.add_field(
+                name=f"Top {subclass_name}", value="\n".join(member_data), inline=True
+            )
 
     # Add data for each subclass
     add_subclass_data("Helm <:wheel:1256589625993068665>", top_helm)
@@ -116,6 +109,7 @@ def create_subclass_leaderboard_embed(bot, guild_id, top_helm, top_flex, top_can
     add_subclass_data("Grenadier <:athenakeg:1030819975730040832>", top_grenadier)
 
     return embed
+
 
 def create_dual_leaderboard_embed(bot, guild_id, data1, title1, data2, title2):
     """
@@ -152,8 +146,16 @@ def create_dual_leaderboard_embed(bot, guild_id, data1, title1, data2, title2):
         member_2 = guild.get_member(member_id_2) if member_id_2 else None
 
         # Construct field values
-        field_value_1 = f"{rank}. {member_1.display_name or member_1.name}: {value1}" if member_1 else f"{rank}."
-        field_value_2 = f"{rank}. {member_2.display_name or member_2.name}: {value2}" if member_2 else f"{rank}."
+        field_value_1 = (
+            f"{rank}. {member_1.display_name or member_1.name}: {value1}"
+            if member_1
+            else f"{rank}."
+        )
+        field_value_2 = (
+            f"{rank}. {member_2.display_name or member_2.name}: {value2}"
+            if member_2
+            else f"{rank}."
+        )
 
         # Add field values to the lists
         field_values_1.append(field_value_1)
