6a53da29f010e66f6f62d91cb40bd2a45d14c8e0
Kip Hart <41959581+kip-hart@users.noreply.github.com>
2025-05-01 09:29:38 -0700

    add daily award check for BOA members (#229)
    * add daily check for BOA members
    
    * missed redefining msg_str
    
    * fix typo
    
    Co-authored-by: Coen Warries <99080092+NoWarries@users.noreply.github.com>
    
    * made award message creation a function
    
    * removed ship var from create function
    
    * input order typo
    
    * de-cluttered create method inputs
    
    * typo in admiralty
    
    ---------
    
    Co-authored-by: Coen Warries <99080092+NoWarries@users.noreply.github.com>

--- src/cogs/tasks/task_check_awards.py
+++ src/cogs/tasks/task_check_awards.py
@@ -7,7 +7,8 @@ from discord.ext import commands, tasks
 from discord.ext.commands.view import StringView
 
 from src.config import MAX_MESSAGE_LENGTH
-from src.config.main_server import GUILD_ID
+from src.config import BOA_ROLE
+from src.config.main_server import GUILD_ID, BC_BOA
 from src.config.ships import SHIPS
 from src.data.repository.sailor_repository import SailorRepository
 from src.utils.check_awards import check_sailor
@@ -16,9 +17,7 @@ from src.utils.discord_utils import alert_engineers
 log = getLogger(__name__)
 
 # Super nasty: reconsider - Trigs
-def fake_context(bot, channel_name="test-channel"):
-    GUILD = bot.get_guild(GUILD_ID)
-
+def fake_context(bot, GUILD, channel_name="test-channel"):
     message = MagicMock(spec=discord.Message)
     message.author = MagicMock(spec=discord.User)
     message.channel = MagicMock(spec=discord.TextChannel)
@@ -30,6 +29,39 @@ def fake_context(bot, channel_name="test-channel"):
     ctx.guild.get_member = GUILD.get_member
     ctx.send = AsyncMock()
     return ctx
+
+def create_award_messages(role, sailor_repo, GUILD, context, exclude_roles=[]):
+    messages = []
+    msg_str = ""
+    for member in role.members:
+        # Get sailor data
+        sailor = sailor_repo.get_sailor(member.id)
+        if sailor is None:
+            continue
+        
+        # Skip if member has any excluded roles
+        exclude = False
+        for ex_role in exclude_roles:
+            if ex_role in sailor.roles:
+                exclude = True
+                break
+        if exclude:
+            continue
+
+        # Check for award messages for sailor
+        sailor_strs = check_sailor(GUILD, context, sailor, member)
+
+        # Add strings to message, printing early if message would be too long
+        for sailor_str in sailor_strs:
+            if len(msg_str + sailor_str) <= MAX_MESSAGE_LENGTH:
+                msg_str += sailor_str
+            else:
+                messages.append(msg_str)
+                msg_str = sailor_str
+    if msg_str:
+        messages.append(msg_str)
+    return messages
+
 
 class AutoCheckAwards(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -45,30 +77,26 @@ class AutoCheckAwards(commands.Cog):
         GUILD = self.bot.get_guild(GUILD_ID)
 
         sailor_repo = SailorRepository()
+        boa_role = GUILD.get_role(BOA_ROLE)
         try:
+            # Ship-assigned members
             for ship in SHIPS:
                 log.info(f"Checking awards for {ship}")
                 channel = GUILD.get_channel(ship.boat_command_channel_id)
                 role = GUILD.get_role(ship.role_id)
-                members = role.members
-                msg_str = ""
-                for member in members:
-                    # Check if member in database
-                    sailor = sailor_repo.get_sailor(member.id)
-                    if sailor is None:
-                        continue
 
-                    # Check for award messages for sailor
-                    sailor_strs = check_sailor(GUILD, fake_context(self.bot, f"{ship.name}"), sailor, member)
-                    # Add strings to message, printing early if message would be too long
-                    for sailor_str in sailor_strs:
-                        if len(msg_str + sailor_str) <= MAX_MESSAGE_LENGTH:
-                            msg_str += sailor_str
-                        else:
-                            await channel.send(msg_str)
-                            msg_str = sailor_str
-                if msg_str:
+                context = fake_context(self.bot, GUILD, f"{ship.name}")
+                award_msgs = create_award_messages(role, sailor_repo, GUILD, context, exclude_roles=[boa_role])
+                for msg_str in award_msgs:
                     await channel.send(msg_str)
+            
+            # BOA members
+            log.info(f"Checking awards for BOA")
+            channel = GUILD.get_channel(BC_BOA)
+            context = fake_context(self.bot, GUILD, "Board of Admiralty")
+            award_msgs = create_award_messages(boa_role, sailor_repo, GUILD, context)
+            for msg_str in award_msgs:
+                await channel.send(msg_str)
 
         except Exception as e:
             log.error(f"Error in AutoCheckAwards: {e}", exc_info=True)
