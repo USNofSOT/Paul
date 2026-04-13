b22caad234b210077b3d0978c3d135107fdaf648
Coen Warries <99080092+NoWarries@users.noreply.github.com>
2026-04-13 22:07:33 +0200

    feat: extend command notification intervals for enhanced scheduling

--- src/config/task_timing.py
+++ src/config/task_timing.py
@@ -5,8 +5,8 @@ from typing import Final
 
 # Daily tasks are intentionally staggered to reduce burst load on the guild and DB.
 TRACK_SHIP_SIZE_TASK_TIME: Final[time] = time(hour=0, minute=3, tzinfo=UTC)
-COMMAND_NOTIFICATION_EVALUATOR_MIN_INTERVAL_HOURS: Final[int] = 6
-COMMAND_NOTIFICATION_EVALUATOR_MAX_INTERVAL_HOURS: Final[int] = 12
+COMMAND_NOTIFICATION_EVALUATOR_MIN_INTERVAL_HOURS: Final[int] = 24
+COMMAND_NOTIFICATION_EVALUATOR_MAX_INTERVAL_HOURS: Final[int] = 26
 CHECK_AWARDS_TASK_TIME: Final[time] = time(hour=15, minute=5, tzinfo=UTC)
 CHECK_TRAINING_AWARDS_TASK_TIME: Final[time] = time(hour=15, minute=12, tzinfo=UTC)
 SHIP_HEALTH_SUMMARY_TASK_TIME: Final[time] = time(hour=9, minute=0, tzinfo=UTC)
